ab8609e928ac0f6672234ce762cb8a1d3667fdd7
João Victor Rossi <jvrossi204@gmail.com>
2022-03-15 00:20:24 -0300

    Pawn Piece Class and Priting on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardchess.Board;
 import boardchess.Piece;
 import boardchess.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -178,7 +179,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.PlacePiece(p, target);
 		
@@ -191,7 +193,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.PlacePiece(p, source);
 		
 		if (capturedPiece != null) {
@@ -204,13 +207,30 @@ public class ChessMatch {
 	
 	
 	private void initialSetup() {
-		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 1, new King(board, Color.WHITE));
+        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
 
-        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('a', 8, new King(board, Color.BLACK));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 8, new King(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
         
 	}
 	
--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,5 +1,77 @@
 package chess.pieces;
 
-public class Pawn {
+import boardchess.Board;
+import boardchess.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece{
+
+	public Pawn(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		if (getColor() == Color.WHITE) {
+			p.setValues(position.getRow() - 1, position.getColumn());
+			if (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p) && getBoard().PositionExists(p2) && !getBoard().TherelsAPiece(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			
+		} else {
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+			if (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p) && getBoard().PositionExists(p2) && !getBoard().TherelsAPiece(p2) && getMoveCount() == 0) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+		}
+		return mat;
+	}
+
+	@Override
+	public String toString() {
+		return "P";
+	}
+	
+	
 
 }
