f677785e1156fdfc78b1f3433c1e51a352a96be3
João Victor Rossi <jvrossi204@gmail.com>
2022-03-15 11:26:20 -0300

    Knight Logic

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -1,5 +1,82 @@
 package chess.pieces;
 
-public class Knight {
+import boardchess.Board;
+import boardchess.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece{
+
+	public Knight(Board board, Color color) {
+		super(board, color);
+	}
+
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		// above
+		p.setValues(position.getRow() -1, position.getColumn() -2);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// below
+		p.setValues(position.getRow() -2 , position.getColumn() -1);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// left
+		p.setValues(position.getRow() -2, position.getColumn() +1);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// right
+		p.setValues(position.getRow() -1, position.getColumn() + 2);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// NW
+		p.setValues(position.getRow() +1 , position.getColumn() +2);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// NE
+		p.setValues(position.getRow() +2 , position.getColumn() + 1);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// SW
+		p.setValues(position.getRow() + 2 , position.getColumn() -1);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// SE
+		p.setValues(position.getRow() +1 , position.getColumn() -2);
+		if (getBoard().PositionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+
+		return mat;
+	}
+	@Override
+	public String toString() {
+		return "N";
+	}
+	
+	
 
 }
