6e0bc0e92c6ad9abc5356fd1dab2950f1b9c2abb
João Victor Rossi <jvrossi204@gmail.com>
2022-03-15 11:36:55 -0300

    Queen Logic

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -1,5 +1,118 @@
 package chess.pieces;
 
-public class Queen {
+import boardchess.Board;
+import boardchess.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece{
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		// above moves
+		p.setValues(position.getRow()-1, position.getColumn());
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// left moves
+		p.setValues(position.getRow(), position.getColumn()-1);
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()-1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// right moves
+		p.setValues(position.getRow(), position.getColumn()+1);
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()+1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// above moves
+		p.setValues(position.getRow()+1, position.getColumn());
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// NW moves
+		p.setValues(position.getRow() -1, position.getColumn() -1);
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);;
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// NE moves
+		p.setValues(position.getRow() -1, position.getColumn() +1);
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() -1, p.getColumn() +1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// SE moves
+		p.setValues(position.getRow() +1, position.getColumn() +1);
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		// SW moves
+		p.setValues(position.getRow() +1, position.getColumn() -1);
+		while (getBoard().PositionExists(p) && !getBoard().TherelsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		}
+		if (getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		
+		return mat;
+	}
+
+		@Override
+		public String toString() {
+			return "Q";
+		}
+		
 
 }
